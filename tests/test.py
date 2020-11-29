ebe4046404016f306b95cc85ac5a2554888a2d3e
maks <m.spiridonov63@gmail.com>
2020-11-29 19:02:39 +0400

    fixed format code

--- tests/test.py
+++ tests/test.py
@@ -26,25 +26,24 @@ async def test_load(conn):
     s = select([country.c.code, country.c.name, country.c.phone_code])
     r = await conn.execute(s)
     assert r is not None
-    s = s.where(country.c.code == 'RU')
+    s = s.where(country.c.code == "RU")
     r = await conn.execute(s)
-    assert await r.fetchone() == ('RU', 'Russia', '7')
+    assert await r.fetchone() == ("RU", "Russia", "7")
 
 
 async def test_search_codes(cli, loop):
-    resp = await cli.post('/search_codes', json={'search_string': 'r'})
+    resp = await cli.post("/search_codes", json={"search_string": "r"})
     assert resp.status == 201
-    assert sorted(await resp.json()) == sorted(['RU', 'RW', 'RE', 'RO', 'CG'])
+    assert sorted(await resp.json()) == sorted(["RU", "RW", "RE", "RO", "CG"])
 
 
 async def test_info_about_country(cli):
-    resp = await cli.get('/info_about_country', params={'code': 'SV'})
+    resp = await cli.get("/info_about_country", params={"code": "SV"})
     assert resp.status == 201
     info = await resp.json()
     assert sorted(info) == sorted(["SV", "El Salvador", "503"])
 
 
 async def test_codes_countries(cli):
-    resp = await cli.get('/codes_countries')
+    resp = await cli.get("/codes_countries")
     assert resp.status == 201
-
